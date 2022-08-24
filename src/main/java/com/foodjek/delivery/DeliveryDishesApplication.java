54dd13b2835af89731f6ca741a9effe13ffd428f
kawtharghariani <kawtharghariani2002@gmail.com>
2022-08-24 12:10:59 +0200

    first commit

---
+++ src/main/java/com/foodjek/delivery/DeliveryDishesApplication.java
@@ -0,0 +1,13 @@
+package com.foodjek.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDishesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodjek/delivery/DeliveryDishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodjek.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDishesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
